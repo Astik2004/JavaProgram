37e6f1e8f1140bc2bdd33fec80ce840fde63f5ac
Astik Yadav <astikyadav4@gmail.com>
2025-11-26 23:01:03 +0530

    Java Code Programming

---
+++ src/oodles/Palinedrom.java
@@ -0,0 +1,28 @@
+package oodles;
+
+public class Palinedrom {
+    static void main(String[] args) {
+        if(isPalindrom(122))
+        {
+            System.out.println("Number is Plaindrom");
+        }
+        else
+        {
+            System.out.println("Number is not plaindrome");
+        }
+    }
+    public static boolean isPalindrom(int n)
+    {
+        //151
+        int num=n;
+        int rev=0;
+        while(n>0)
+        {
+            int d=n%10;
+            rev=rev*10+d;
+            n=n/10;
+        }
+
+        return rev==num;
+    }
+}
