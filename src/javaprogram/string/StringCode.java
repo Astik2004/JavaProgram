f153c76fd36940e14e77e3dd8671368c6a1e666b
Astik Yadav <astikyadav4@gmail.com>
2025-12-08 19:55:37 +0530

    Java String Code Programming Reverse word in String

--- src/javaprogram/string/StringCode.java
+++ src/javaprogram/string/StringCode.java
@@ -2,6 +2,23 @@ package javaprogram.string;
 
 public class StringCode {
     public static void main(String[] args) {
-
+        String s="Python is programming language";
+        String v="AaEeIiOoUu";
+        StringBuilder sb=new StringBuilder();
+        boolean [] seen=new boolean[256];
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(v.indexOf(c)!=-1)
+            {
+                continue;
+            }
+            if(!seen[c])
+            {
+                sb.append(c);
+                seen[c]=true;
+            }
+        }
+        System.out.println(sb);
     }
 }
