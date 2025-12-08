b25b03b6d87f8ccee9d7830345b6c678ffd21ff9
Astik Yadav <astikyadav4@gmail.com>
2025-12-08 19:44:23 +0530

    Java String Code Programming Reverse word in String

--- src/javaprogram/string/ReverseWord.java
+++ src/javaprogram/string/ReverseWord.java
@@ -2,6 +2,35 @@ package javaprogram.string;
 
 public class ReverseWord {
     public static void main(String[] args) {
+        String s="  Astik Kumar Yadav PBH                  ";
+        String res="";
+        String word="";
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            char c=s.charAt(i);
+            if(c!=' ')
+            {
+                word=c+word;
+            }
+            else {
+                if (!word.isEmpty()) {
+                    if(res.isEmpty())
+                    {
+                        res=word;
+                        word="";
+                    }
+                    else {
+                        res = res+ " " + word;
+                        word = "";
+                    }
+                }
+            }
 
+        }
+        if(!word.isBlank())
+        {
+            res=res+" "+word;
+        }
+        System.out.println(res);
     }
 }
