5f3268d993f44bd4a94b77660656eda54bef77e7
Astik Yadav <astikyadav4@gmail.com>
2025-12-14 20:03:25 +0530

    Java Practice

--- src/javaprogram/program/MoveAllZeroToleft.java
+++ src/javaprogram/program/MoveAllZeroToleft.java
@@ -1,26 +1,47 @@
 package javaprogram.program;
 
+import java.util.Arrays;
+
 public class MoveAllZeroToleft {
     public static void main(String[] args) {
-        String s="a#bcde#def#";
-        char [] arr=s.toCharArray();
-        int left=0;
-        int n=arr.length;
-        int t=-1;
-        while(left<n)
+//        String s="a#bcde#def#";
+//        char [] arr=s.toCharArray();
+//        int left=0;
+//        int n=arr.length;
+//        int t=-1;
+//        while(left<n)
+//        {
+//            if(arr[left]=='#')
+//            {
+//                t++;
+//                char temp=arr[left];
+//                arr[left]=arr[t];
+//                arr[t]=temp;
+//                left++;
+//            }
+//            else {
+//                left++;
+//            }
+//        }
+//        System.out.println(arr);
+        int [] a={1,0,2,3,0,4,0};
+        int s=0;
+        int i=0;
+        while(i<a.length)
         {
-            if(arr[left]=='#')
+            if(a[i]!=0)
             {
-                t++;
-                char temp=arr[left];
-                arr[left]=arr[t];
-                arr[t]=temp;
-                left++;
+                int t=a[i];
+                a[i]=a[s];
+                a[s]=t;
+                s++;
+                i++;
             }
-            else {
-                left++;
+            else
+            {
+                i++;
             }
         }
-        System.out.println(arr);
+        System.out.println(Arrays.toString(a));
     }
 }
