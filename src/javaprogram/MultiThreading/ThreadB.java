474a7af6a507f1bec579ffcf711554425ed6c1b5
Astik Yadav <astikyadav4@gmail.com>
2025-12-28 23:24:20 +0530

    Java Practice Stream API

--- src/javaprogram/MultiThreading/ThreadB.java
+++ src/javaprogram/MultiThreading/ThreadB.java
@@ -3,8 +3,9 @@ package javaprogram.MultiThreading;
 public class ThreadB extends Thread {
 
     public void run() {
+        System.out.println("Thread Name : "+Thread.currentThread().getName());
         System.out.println("Thread Execution Starting...........");
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <=5; i++) {
             try {
                 System.out.println("Astik Yadav");
                 Thread.sleep(1000);
@@ -17,6 +18,10 @@ public class ThreadB extends Thread {
 
     public static void main(String[] args) {
         ThreadB tb = new ThreadB();
-        tb.start();   // starts a new thread
+        tb.setName("tb");
+        tb.start();
+        ThreadB tb1=new ThreadB();
+        tb.setName("tb1");
+        tb1.start();
     }
 }
