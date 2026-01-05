e3b8be92e70806b64cec5925c5661ca9621b720a
Astik Yadav <astikyadav4@gmail.com>
2026-01-05 23:14:02 +0530

    Singal Linked List Code more

--- src/CoreDSA/linklist/LinkedListDriver.java
+++ src/CoreDSA/linklist/LinkedListDriver.java
@@ -3,13 +3,31 @@ package CoreDSA.linklist;
 public class LinkedListDriver {
     public static void main(String[] args) {
         SingleLinkedList sll=new SingleLinkedList();
+        //Add at Last
+        System.out.println("Add from Last");
         sll.insertAtEnd(10);
         sll.insertAtEnd(11);
         sll.insertAtEnd(12);
         sll.insertAtEnd(13);
         sll.insertAtEnd(14);
         sll.insertAtEnd(15);
-
+        sll.printList();
+        //Add at Begining
+        System.out.println("Add from Begining");
+        sll.insertAtBegining(9);
+        sll.insertAtBegining(8);
+        sll.printList();
+        //Insert at particuler position
+        System.out.println("Inserting At perticuler position");
+        sll.insertAtPosition(100,3);
+        sll.printList();
+        // Delete from Begining
+        System.out.println("Delete from begining");
+        sll.deleteFromBeginning();
+        sll.printList();
+        //Delete from End
+        System.out.println("Delete from End");
+        sll.deleteFromEnd();
         sll.printList();
     }
 }
--- src/CoreDSA/linklist/SingleLinkedList.java
+++ src/CoreDSA/linklist/SingleLinkedList.java
@@ -2,6 +2,12 @@ package CoreDSA.linklist;
 
 public class SingleLinkedList {
     Node head;
+    public void insertAtBegining(int data)
+    {
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+    }
     public void insertAtEnd(int data)
     {
         Node newNode=new Node(data);
@@ -18,6 +24,43 @@ public class SingleLinkedList {
         }
         temp.next=newNode;
     }
+    public void insertAtPosition(int data,int pos)
+    {
+        Node newNode=new Node(data);
+        if(pos==1)
+        {
+            insertAtBegining(data);
+            return;
+        }
+        Node temp=head;
+        for(int i=1;i<pos-1;i++)
+        {
+            temp=temp.next;
+        }
+        newNode.next=temp.next;
+        temp.next=newNode;
+    }
+    public void deleteFromBeginning()
+    {
+        if(head!=null)
+        {
+            head=head.next;
+        }
+    }
+    public void deleteFromEnd()
+    {
+        if(head==null || head.next==null)
+        {
+            head=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=null;
+    }
     public void printList()
     {
         Node temp=head;
@@ -27,5 +70,6 @@ public class SingleLinkedList {
             temp=temp.next;
         }
         System.out.print("null");
+        System.out.println();
     }
 }
